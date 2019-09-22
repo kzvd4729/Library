da79da189e0153ae5a3edb768d6bf154bf76f4e0
kzvd4729 <kzvd4729@gmail.com>
2019-09-22 18:20:09 +0600

    Update fft.cpp

--- fft.cpp
+++ fft.cpp
@@ -40,58 +40,55 @@ eighth roots of unity are 45 degree far from each other.
 so we represent complex number by their polar angle cos+isin.
 we find the angle of the roots of unity, multiply to rotate point uniformly.
 */
-struct fastFourierTransform
+const double pi=acos(-1.0);
+struct complx
 {
-  const double pi=acos(-1.0);
-  struct complex
-  {
-    double x,y;
-    complex(double a=0,double b=0):x(a),y(b){}
-    complex operator+(complex c){return complex(x+c.x,y+c.y);}
-    complex operator-(complex c){return complex(x-c.x,y-c.y);}
-    complex operator*(complex c){return complex(x*c.x-y*c.y,x*c.y+y*c.x);}//complex
-    complex operator/(double d){return complex(x/d,y/d);}
-    void operator*=(complex c){double a=x*c.x-y*c.y,b=x*c.y+y*c.x;x=a,y=b;}
-    void operator/=(double d){x/=d,y/=d;}
-  };
-  void dft(vector<complex>&a,bool inv)
-  {
-    int n=a.size();if(n==1)return ;
-    vector<complex>a0(n/2),a1(n/2);
-
-    for(int i=0;i<n/2;i++)
-      a0[i]=a[2*i],a1[i]=a[2*i+1];
-    dft(a0,inv);dft(a1,inv);
-
-    double ang=2*pi/n*(inv?-1:1);
-    complex r(1),m(cos(ang),sin(ang));
-    for(int i=0;i<n/2;i++)
-    {
-      a[i]=a0[i]+r*a1[i];
-      a[i+n/2]=a0[i]-r*a1[i];
-      if(inv)
-        a[i]/=2,a[i+n/2]/=2;//overall divided by n
-      r*=m;
-    }
-  }
-  //polynomial of the form a0x^0+a1x^1+a2x^2+a3x^3+.....
-  vector<int>multiply(vector<int>&a,vector<int>&b)
-  {
-    vector<complex>fa(a.begin(),a.end());//all real part
-    vector<complex>fb(b.begin(),b.end());//all real part
-    int n=1;while(n<a.size()+b.size())n*=2;
-    fa.resize(n);fb.resize(n);//padding higher degree with 0 coefficient
-
-    dft(fa,false);dft(fb,false);//now in sample form
-
-    for(int i=0;i<n;i++)//scalar operation on sample
-      fa[i]*=fb[i];
-
-    dft(fa,true);//inverse transform
-
-    vector<int>ret(n);
-    for(int i=0;i<n;i++)
-      ret[i]=round(fa[i].x);
-    return ret;
-  }
+  double x,y;
+  complx(double a=0,double b=0):x(a),y(b){}
+  complx operator+(complx c){return complx(x+c.x,y+c.y);}
+  complx operator-(complx c){return complx(x-c.x,y-c.y);}
+  complx operator*(complx c){return complx(x*c.x-y*c.y,x*c.y+y*c.x);}//complx
+  complx operator/(double d){return complx(x/d,y/d);}
+  void operator*=(complx c){double a=x*c.x-y*c.y,b=x*c.y+y*c.x;x=a,y=b;}
+  void operator/=(double d){x/=d,y/=d;}
 };
+void dft(vector<complx>&a,bool inv)
+{
+  int n=a.size();if(n==1)return ;
+  vector<complx>a0(n/2),a1(n/2);
+
+  for(int i=0;i<n/2;i++)
+    a0[i]=a[2*i],a1[i]=a[2*i+1];
+  dft(a0,inv);dft(a1,inv);
+
+  double ang=2*pi/n*(inv?-1:1);
+  complx r(1),m(cos(ang),sin(ang));
+  for(int i=0;i<n/2;i++)
+  {
+    a[i]=a0[i]+r*a1[i];
+    a[i+n/2]=a0[i]-r*a1[i];
+    if(inv)
+      a[i]/=2,a[i+n/2]/=2;//overall divided by n
+    r*=m;
+  }
+}
+//polynomial of the form a0x^0+a1x^1+a2x^2+a3x^3+.....
+vector<int>multiply(vector<int>&a,vector<int>&b)
+{
+  vector<complx>fa(a.begin(),a.end());//all real part
+  vector<complx>fb(b.begin(),b.end());//all real part
+  int n=1;while(n<a.size()+b.size())n*=2;
+  fa.resize(n);fb.resize(n);//padding higher degree with 0 coefficient
+
+  dft(fa,false);dft(fb,false);//now in sample form
+
+  for(int i=0;i<n;i++)//scalar operation on sample
+    fa[i]*=fb[i];
+
+  dft(fa,true);//inverse transform
+
+  vector<int>ret(n);
+  for(int i=0;i<n;i++)
+    ret[i]=round(fa[i].x);
+  return ret;
+}
