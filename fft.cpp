87f96dedcb6882b8577471c990bffd5e09d848cf
kzvd4729 <kzvd4729@gmail.com>
2019-09-22 19:13:32 +0600

    Update fft.cpp

--- fft.cpp
+++ fft.cpp
@@ -72,6 +72,47 @@ void dft(vector<complx>&a,bool inv)
     r*=m;
   }
 }
+/*
+turns out we convert divide and conquer algorithm into an iterative one.
+in first recursion level last bit 0 element will be in first half and other in
+second half. this is true for other recursion level as well.
+so we can shuffle the order of the element by reversing the bit so that every
+recursion level becomes continuous and thus allow iterative implementation
+*/
+vector<int>r;
+void bitReverse(int n)//n is power of 2
+{
+  r.resize(n,0);
+  int p=0;while((1<<p)<n)p++;
+  for(int i=0;i<n;i++)
+  {
+    for(int j=0;j<p;j++)
+      if(i&(1<<j))r[i]|=(1<<(p-j-1));
+  }
+}
+void dft(vector<complx>&a,bool inv)
+{
+  int n=a.size();
+  for(int i=0;i<n;i++)
+    if(r[i]<i)swap(a[i],a[r[i]]);
+
+  for(int ln=2;ln<=n;ln*=2)//1 doesn't need to be calculated.
+  {
+    double ang=2*pi/ln*(inv?-1:1);
+    complx m(cos(ang),sin(ang));//multiplier, roots of unity
+    for(int i=0;i<n;i+=ln)//every ln length segment in this level
+    {
+      complx r(1),u,v;//unit angle
+      for(int j=0;j<ln/2;j++)
+      {
+        u=a[i+j],v=r*a[i+j+ln/2];//even and odd
+        a[i+j]=u+v,a[i+j+ln/2]=u-v;//positive in the first half
+        r*=m;
+      }
+    }
+  }
+  if(inv)for(auto &x:a)x/=n;
+}
 //polynomial of the form a0x^0+a1x^1+a2x^2+a3x^3+.....
 vector<int>multiply(vector<int>&a,vector<int>&b)
 {
@@ -79,6 +120,8 @@ vector<int>multiply(vector<int>&a,vector<int>&b)
   vector<complx>fb(b.begin(),b.end());//all real part
   int n=1;while(n<a.size()+b.size())n*=2;
   fa.resize(n);fb.resize(n);//padding higher degree with 0 coefficient
+
+  bitReverse(n);//for iterative implementation
 
   dft(fa,false);dft(fb,false);//now in sample form
 
